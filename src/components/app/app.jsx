a2f4450d8fd1fe164347a9918c3260f47e8b3a78
Natalia Zaytseva <zaycevan@gmail.com>
2020-10-01 21:41:18 +0500

    Установлены пакеты. Настроен Webpack.

---
+++ src/components/app/app.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import PropTypes from "prop-types";
+import WelcomeScreen from "../welcome-screen/welcome-screen";
+
+const App = (props) => {
+  const {errorsCount} = props;
+
+  return (
+    <WelcomeScreen errorsCount={errorsCount} />
+  );
+};
+
+App.propTypes = {
+  errorsCount: PropTypes.number.isRequired,
+};
+
+export default App;
